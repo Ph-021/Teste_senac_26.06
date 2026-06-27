20149f3d10eb09b563e23b9f8271437f265e2aef
Raphael <raphael021santos@gmail.com>
2026-06-26 21:45:31 -0300

    atualização da aula26.06

--- teste.py
+++ teste.py
@@ -1,1 +1,30 @@
-print("oi")
+'''print("oi")'''
+
+'''print("Olá, aluno!")
+nome = (input("Digite seu Nome: "))
+idade = int(input(f'{nome}, qual é a sua idade? '))
+n1 = float(input("Qual foi a sua primeira nota? "))
+n2 = float(input("Qual foi a sua segunda nota? "))
+
+media = (n1 + n2) / 2
+
+if media >= 7.0:
+    print("Aprovado")
+elif media >= 5.0:
+    print("Recuperação")
+else:
+    print("Reprovado")'''
+
+
+
+a5kg = 24.90
+f1k = 8.50
+m500g = 4.30
+mtomate = 3.70
+oleo900 = 8.90
+
+print("")
+
+resp_arroz = input('comprar arroz ? (S/N) ')
+
+if resp_arroz == 24.90
